c349282e3e5b3bc3aeab02a9909bc4f836688654
Fefe <fedepalo13@gmail.com>
2024-04-22 09:39:59 -0300

    Fixed reports (#3)
    * fixed reports
    
    * removed unused imports
    
    * fixed firefox and removed webkit
    
    * removed webkit from pipeline
    
    * updated playwright version

--- src/core/hooks.ts
+++ src/core/hooks.ts
@@ -1,40 +1,39 @@
 import { After, AfterAll, AfterStep, Before, Status } from "@cucumber/cucumber";
-import { Browser, BrowserContext, chromium, firefox, webkit } from "@playwright/test";
+import { Browser, chromium, firefox, webkit } from "@playwright/test";
 import { finalizeCoverage, saveV8Coverage } from "./coverageHelper";
 import { pageFixture } from "./pageFixture";
 
 let browser : Browser;
-let context: BrowserContext;
-let page;
+let worldContext: any;
 
 Before(async function(scenario) {
     try {
-        console.log(`Starting scenario: ${scenario.pickle.name}`);
-        
+        // Determine which browser to us
+        await console.log(`Starting scenario: ${scenario.pickle.name}`);
+        worldContext = this;
+
         // Determine which browser to use
-        const browserType = process.env.BROWSER_TYPE || 'chromium'; // Default to chromium if no env variable is set
-        const isHeadless = process.env.HEADLESS === 'true'; // Check the HEADLESS environment variable
+        let browserType = process.env.BROWSER_TYPE; // Default to chromium if no env variable is set
+        let isHeadless = process.env.HEADLESS === 'true'; // Check the HEADLESS environment variable
 
         let launchOptions = {
             headless: isHeadless,
             args: ['--start-maximized'] // Maximized window is only relevant in non-headless mode
         };
 
-        let browser;
-        switch (browserType.toLowerCase()) {
-            case 'firefox':
-                browser = await firefox.launch(launchOptions);
-                break;
-            case 'webkit':
-                browser = await webkit.launch(launchOptions);
-                break;
-            default:
-                browser = await chromium.launch(launchOptions);
-                break;
+        if(browserType){
+            switch (browserType.toLowerCase()) {
+                case 'firefox':
+                    browser = await firefox.launch(launchOptions);
+                    break;
+                default:
+                    browser = await chromium.launch(launchOptions);
+                    break;
+            }
         }
-
-        context = await browser.newContext();
-        page = await context.newPage();
+        
+        const context = await browser.newContext();
+        const page = await context.newPage();
         pageFixture.page = page;
         //await pageFixture.page.coverage.startJSCoverage();
     } catch (error) {
@@ -51,24 +50,12 @@ AfterStep(async function({pickle, result}) {
 });
 
 After(async function () {
-    try {
-        if (pageFixture.page && !pageFixture.page.isClosed()) {
-            console.log('Closing page...');
-            await pageFixture.page.close();
-        }
-        if (context) {
-            console.log('Closing context...');
-            await context.close();
-        }
-    } finally {
-        if (browser && browser.isConnected()) {
-            console.log('Closing browser...');
-            await browser.close();
-        }
-    }
+    //await saveV8Coverage(pageFixture.page);
+    await pageFixture.page.close();
+    await browser.close();
 });
 
+
 AfterAll(async () => {
-    process.exit(0);
     //await finalizeCoverage();
 });
