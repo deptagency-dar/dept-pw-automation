623f92481b1194e707c31e81627245f2f2ff3cfb
Facundo97Maldonado <facumaldonado319@gmail.com>
2024-05-06 16:01:05 -0300

    Adding browsers and pipeline setup (#9)
    * added browsers chrome, edge, mobilechrome, mobilesafari and webkit. also setup pipeline
    
    * checking updates for browsers on pipeline
    
    * updated main.yml
    
    * re-update main.yml - checking github pipeline
    
    * main.yml - run npx command change
    
    * main.yml - npx command
    
    * start maximized removed

--- README.md
+++ README.md
@@ -5,9 +5,14 @@ Playwright automation framework
 
 > npm install
 
-> npx playwright install
+> npx playwright@latest install
 
 # Run scripts:
 
 > npm run test:chromium          
 > npm run test:firefox
+> npm run test:chrome
+> npm run test:edge
+> npm run test:mobilechrome
+> npm run test:mobilesafari
+> npm run test:webkit
--- src/core/hooks.ts
+++ src/core/hooks.ts
@@ -1,10 +1,13 @@
 import { After, AfterAll, AfterStep, Before, Status } from "@cucumber/cucumber";
-import { Browser, chromium, firefox, webkit } from "@playwright/test";
+import { Browser, chromium, firefox, webkit, devices } from "@playwright/test";
 import { finalizeCoverage, saveV8Coverage } from "./coverageHelper";
 import { pageFixture } from "./pageFixture";
+import { exec } from "child_process";
+import { channel } from "diagnostics_channel";
 
 let browser : Browser;
 let worldContext: any;
+
 
 Before(async function(scenario) {
     try {
@@ -18,13 +21,52 @@ Before(async function(scenario) {
 
         let launchOptions = {
             headless: isHeadless,
-            args: ['--start-maximized'] // Maximized window is only relevant in non-headless mode
+            //args: ['--start-maximized'] // Maximized window is only relevant in non-headless mode
         };
 
         if(browserType){
             switch (browserType.toLowerCase()) {
                 case 'firefox':
-                    browser = await firefox.launch(launchOptions);
+                    const firefoxDesktop = devices['Desktop Firefox'];
+                    browser = await firefox.launch({
+                        ...launchOptions,
+                        ...firefoxDesktop,
+                    });
+                    break;
+                case 'chrome':
+                    const chromeDesktop = devices['Desktop Chrome'];
+                    browser = await chromium.launch({
+                        ...launchOptions,
+                        ...chromeDesktop,
+                    });
+                    break;
+                case 'edge':
+                    const edgeDesktop = devices['Desktop Edge'];
+                    browser = await chromium.launch({
+                        ...launchOptions,
+                        ...edgeDesktop,
+                    });
+                    break;
+                case 'mobilechrome':
+                    const Pixel2 = devices['Pixel 2'];
+                    browser = await webkit.launch({
+                        ...launchOptions,
+                        ...Pixel2
+                    });
+                    break;
+                case 'mobilesafari':
+                    const iPhone11 = devices['iPhone 11'];
+                    browser = await webkit.launch({
+                        ...launchOptions,
+                        ...iPhone11,
+                    });
+                    break;
+                case 'webkit':
+                    const safariDesktop = devices['Desktop Safari'];
+                    browser = await webkit.launch({
+                        ...launchOptions,
+                        ...safariDesktop,
+                    });
                     break;
                 default:
                     browser = await chromium.launch(launchOptions);
@@ -33,11 +75,12 @@ Before(async function(scenario) {
         }
         
         const context = await browser.newContext();
-        const page = await context.newPage();
-        pageFixture.page = page;
+        pageFixture.page = await context.newPage();
+
         //await pageFixture.page.coverage.startJSCoverage();
     } catch (error) {
-        console.log(error);
+        console.error("Error initializing browser: " + error);
+        throw error;
     }
 });
 
@@ -51,11 +94,36 @@ AfterStep(async function({pickle, result}) {
 
 After(async function () {
     //await saveV8Coverage(pageFixture.page);
-    await pageFixture.page.close();
-    await browser.close();
+    try {
+        // Check if browser exists before attempting to close it
+        if (browser) {
+            await browser.close();
+        } else {
+            console.log("Browser is not open or already closed.");
+        }
+    } catch (error) {
+        console.error("Error closing browser: " + error);
+        throw error; // Rethrow the error to fail the test if closing fails
+    }
+
+    try {
+        // Check if page and browserContext exist before attempting to close them
+        if (pageFixture.page && !pageFixture.page.isClosed()) {
+            await pageFixture.page.close();
+        }
+    } catch (error) {
+        console.error("Error closing page and browser context: " + error);
+        throw error; // Rethrow the error to fail the test if closing fails
+    }
 });
 
 
 AfterAll(async () => {
-    //await finalizeCoverage();
+    try {
+        await finalizeCoverage();
+        exec("pkill -f playwright");
+    } catch (error) {
+        console.error("Error during test teardown: " + error);
+        throw error; // Relaunch the error to fail the test if closing fails
+    }
 });
