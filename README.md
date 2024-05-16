3a49c22133b332e93182587ced7bac74b41304ef
gbarrionuevoml <gbarrionuevo@meazurelearning.com>
2024-05-16 13:03:17 -0300

    improvements

--- README.md
+++ README.md
@@ -22,13 +22,3 @@ Playwright automation framework
 > npm run test:mobilesafari
 
 > npm run test:webkit
-
-# Workaround login (Generating auth.json file)
-
-> run `npx playwright codegen --save-storage=auth.json`
-> It will open Chromium
-> Paste the dept-central URL: https://dept-central-client-mlbp.vercel.app/
-> Login manually
-> Home page will be displayed
-> Close the browser
-> Storage will be saved on `auth.json` file
--- src/test/steps/deptCentralSteps.ts
+++ src/test/steps/deptCentralSteps.ts
@@ -9,10 +9,6 @@ Given('I go to {string}', {timeout:20000}, async function (website: string) {
     homePage = new HomePage(pageFixture.page);
 });
 
-Then('I can see that the title {string} is displayed and matches', {}, async function (title) {
-    await homePage.isVisibleTimeOffTitle();
-});
-
-Then('I can see the subtitle {string} is displayed and matches', {}, async function (subtitle) {
-    await homePage.isVisibleTimeOffSubtitle();
+Then('I can see that the {string} is displayed', {}, async function (element) {
+    await homePage.validateTextElements(element);
 });
